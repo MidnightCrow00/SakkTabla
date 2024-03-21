2edd1b3d3d30d9e36b23d5b332ea501c31d0581f
MidnightCrow00 <orsolyaalbert1@gmail.com>
2024-03-21 14:07:37 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+import { htmlOsszeAllit } from "./fuggveny.js";
+
+//elemek elérése
+
+document.getElementByID("idvelmegadottnev")
+
+doucument. querySelectorAll("tetszolegesCssSzelector")[index]
+
+document.getElementsByClassName("classnevvelmegadottnev")[0]
+
+
+
+//eseménykezelők regisztrálása
+
+ 
+elemnev. addEventListener("click",fvenynev)
+
+
+elemnev.style.border="1px solid gray"
+
+elemnev.className="cssosztalynev"
+
+ 
